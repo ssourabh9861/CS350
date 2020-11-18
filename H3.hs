6d2fd3625d0c1a9205690494c6be09a91ca44f0a
ssourabh9861 <sourabhkr9861@gmail.com>
2020-11-19 00:11:50 +0530

    submitted H3

--- H3.hs
+++ H3.hs
@@ -9,11 +9,8 @@ uniq [] = []
 uniq (x:xs) = x:(uniq [y|y<-xs, y/=x])
 
 --Solution of Question 3
--- neighbors :: (Ord a1, Ord a2, Num a1, Num a2) => a1 -> a2 -> [(a1, a2)]
-change_val = [(x,y)|x<-[-1,1,0], y<-[-1,1,0], x/=0 || y/=0]
 condition (x,y) = (x>=0) && (x<10) && (y>=0) && (y<10)
-neighbors x y = quickSort (filter condition [(a+x, b+y)|(a,b)<-change_val])
-
+neighbours x y =  quickSort (filter condition [(t1,t2) | t1 <- [x-1, x, x+1], t2 <- [y-1, y, y+1]])
 
 
 --Solution of Question 4
